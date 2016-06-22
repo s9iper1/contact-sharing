f311a5c05205a041d34368bc23430f6a53cfdae9
s9iper1 <s9iper1@gmail.com>
2016-06-22 19:24:54 +0500

    remove extra code

--- app/src/main/java/com/byteshaft/contactsharing/CardDetailsActivity.java
+++ app/src/main/java/com/byteshaft/contactsharing/CardDetailsActivity.java
@@ -1,7 +1,6 @@
 package com.byteshaft.contactsharing;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -10,23 +9,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.byteshaft.contactsharing.bluetooth.BluetoothActivity;
 import com.byteshaft.contactsharing.database.CardsDatabase;
 import com.byteshaft.contactsharing.utils.AppGlobals;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 
 
-public class CardDetailsActivity extends Activity implements View.OnClickListener {
+public class CardDetailsActivity extends Activity {
 
     private int cardId;
     private CardsDatabase cardsDatabase;
@@ -39,9 +32,6 @@ public class CardDetailsActivity extends Activity implements View.OnClickListene
     private TextView organization;
     private TextView jobzyId;
     private ImageView cardImage;
-    private FrameLayout frameLayout;
-    private ImageButton editButton;
-    private ImageButton shareButton;
     private RelativeLayout mainLayout;
     private String color;
     private Uri imgUri;
@@ -58,14 +48,8 @@ public class CardDetailsActivity extends Activity implements View.OnClickListene
         address = (TextView) findViewById(R.id.location);
         organization = (TextView) findViewById(R.id.tv_organization);
         jobzyId = (TextView) findViewById(R.id.tv_jobzy_id);
-        frameLayout = (FrameLayout) findViewById(R.id.buttons);
-        editButton = (ImageButton) findViewById(R.id.edit_button);
-        shareButton = (ImageButton) findViewById(R.id.share_button);
         cardImage = (ImageView) findViewById(R.id.card_image);
         mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
-
-        editButton.setOnClickListener(this);
-        shareButton.setOnClickListener(this);
         cardsDatabase = new CardsDatabase(getApplicationContext());
 
         cardId = getIntent().getIntExtra(AppGlobals.CARD_ID, 0);
@@ -102,7 +86,6 @@ public class CardDetailsActivity extends Activity implements View.OnClickListene
             jobzyId.setTypeface(AppGlobals.regularTypeface);
         } else if (cardData.get(AppGlobals.IS_IMAGE).equals("1")) {
             mainLayout.setBackgroundColor(Color.TRANSPARENT);
-            editButton.setVisibility(View.GONE);
             personName.setVisibility(View.GONE);
             jobTitle.setVisibility(View.GONE);
             phoneNumber.setVisibility(View.GONE);
@@ -125,11 +108,7 @@ public class CardDetailsActivity extends Activity implements View.OnClickListene
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (frameLayout.getVisibility() == View.VISIBLE) {
-                    frameLayout.setVisibility(View.GONE);
-                } else {
-                    frameLayout.setVisibility(View.VISIBLE);
-                }
+
                 return true;
         }
         return super.onTouchEvent(event);
@@ -141,45 +120,45 @@ public class CardDetailsActivity extends Activity implements View.OnClickListene
         finish();
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.edit_button:
-                Intent editIntent = new Intent(CardDetailsActivity.this, BusinessForm.class);
-                editIntent.putExtra("id", cardId);
-                System.out.println(cardId);
-                startActivity(editIntent);
-                break;
-            case R.id.share_button:
-                JSONObject jsonObject = new JSONObject();
-                if (isImage) {
-                    try {
-                        jsonObject.put(AppGlobals.IS_IMAGE_SHARE, 1);
-                        jsonObject.put(AppGlobals.NAME, personName.getText().toString());
-                        jsonObject.put(AppGlobals.IMG_URI, cardData.get(AppGlobals.IMG_URI));
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                } else {
-                    try {
-                        jsonObject.put(AppGlobals.IS_IMAGE_SHARE, 0);
-                        jsonObject.put(AppGlobals.NAME, personName.getText().toString());
-                        jsonObject.put(AppGlobals.ADDRESS, address.getText().toString());
-                        jsonObject.put(AppGlobals.EMAIL, emailAddress.getText().toString());
-                        jsonObject.put(AppGlobals.JOB_TITLE, jobTitle.getText().toString());
-                        jsonObject.put(AppGlobals.ORG, organization.getText().toString());
-                        jsonObject.put(AppGlobals.JOBZY_ID, jobzyId.getText().toString());
-                        jsonObject.put(AppGlobals.NUMBER, phoneNumber.getText().toString());
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                finish();
-                Intent intent = new Intent(getApplicationContext(), BluetoothActivity.class);
-                intent.putExtra(AppGlobals.DATA_TO_BE_SENT, jsonObject.toString());
-                startActivity(intent);
-                break;
-        }
-
-    }
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.edit_button:
+//                Intent editIntent = new Intent(CardDetailsActivity.this, BusinessForm.class);
+//                editIntent.putExtra("id", cardId);
+//                System.out.println(cardId);
+//                startActivity(editIntent);
+//                break;
+//            case R.id.share_button:
+//                JSONObject jsonObject = new JSONObject();
+//                if (isImage) {
+//                    try {
+//                        jsonObject.put(AppGlobals.IS_IMAGE_SHARE, 1);
+//                        jsonObject.put(AppGlobals.NAME, personName.getText().toString());
+//                        jsonObject.put(AppGlobals.IMG_URI, cardData.get(AppGlobals.IMG_URI));
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                } else {
+//                    try {
+//                        jsonObject.put(AppGlobals.IS_IMAGE_SHARE, 0);
+//                        jsonObject.put(AppGlobals.NAME, personName.getText().toString());
+//                        jsonObject.put(AppGlobals.ADDRESS, address.getText().toString());
+//                        jsonObject.put(AppGlobals.EMAIL, emailAddress.getText().toString());
+//                        jsonObject.put(AppGlobals.JOB_TITLE, jobTitle.getText().toString());
+//                        jsonObject.put(AppGlobals.ORG, organization.getText().toString());
+//                        jsonObject.put(AppGlobals.JOBZY_ID, jobzyId.getText().toString());
+//                        jsonObject.put(AppGlobals.NUMBER, phoneNumber.getText().toString());
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                finish();
+//                Intent intent = new Intent(getApplicationContext(), BluetoothActivity.class);
+//                intent.putExtra(AppGlobals.DATA_TO_BE_SENT, jsonObject.toString());
+//                startActivity(intent);
+//                break;
+//        }
+//
+//    }
 }
